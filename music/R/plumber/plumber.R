7a7798eef69030b51a8352364f7731e9c17bd545
James Blair <james.m.blair09@gmail.com>
2022-07-26 14:34:07 -0400

    Update Plumber API to only include Random Forest

--- music/R/plumber/plumber.R
+++ music/R/plumber/plumber.R
@@ -2,26 +2,16 @@ library(plumber)
 library(plumbertableau)
 library(ranger)
 
-#* @apiTitle Regression Models
-#* @apiDescription Fit simple regression models to input data
-
-#* Fit linear regression to input data
-#* @tableauArg x:numeric independent variable
-#* @tableauArg y:numeric dependent variable
-#* @tableauReturn numeric Predicted output from fit model
-#* @post /lr
-lm_fit <- function(x, y) {
-  fit <- lm(y ~ x)
-  predict(fit)
-}
+#* @apiTitle Tableau Random Forest
+#* @apiDescription Fit simple random forest model to input data
 
 #* Fit random forest to input data
 #* @tableauArg x:numeric independent variable
-#* @tableauArg y:numeric dependent variable
+#* @tableauArg y:any dependent variable
 #* @tableauReturn numeric Predicted output from fit model
 #* @post /rf
 rf_fit <- function(x, y) {
-  train_data <- tibble(x = x, y = y)
+  train_data <- data.frame(x = x, y = y)
   fit <- ranger(y ~ x, data = train_data)
   predictions(fit)
 }
